3f30a43fbdd75bf0b8651e49cdfbe248d37ce444
ayushkumar107 <ayushbaranwal1472@gmail.com>
2026-05-09 21:13:47 +0530

    changed file name

---
+++ loops.java
@@ -0,0 +1,22 @@
+public class loops {
+   
+   
+    public static void main(String[] args) {
+      //for loop 1 to 5
+      
+    //     for(int n=1;n<=5;n++){
+    //     System.out.println(n);
+    //    }
+
+
+
+    //loop 1 to n
+
+    
+
+
+
+
+
+    }
+}
